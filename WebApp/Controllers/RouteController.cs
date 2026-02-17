bf78bf69373ef88d5184894f8b8b379e5d4638e8
Christen <christen.foad@gmail.com>
2026-02-17 11:56:19 +0200

    Day7

---
+++ WebApp/Controllers/RouteController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    //r/Method1
+    //r/Method2
+    
+
+    public class RouteController : Controller
+    {
+        //Route/Method1
+        //r1?name=ahmed&id=12
+        //r1/12/ahmed
+        //r1/22/chtisten
+        //r1/22
+        public IActionResult Method1(int id,string name)
+        {
+            return Content("M1");
+        }
+
+        //Route/Method2
+        //r2
+        public IActionResult Method2()
+        {
+            return Content("M2");
+        }
+    }
+}
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -8,7 +8,7 @@ namespace WebApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            #region Services
             // Add services to the container.
             // buit in service already register
             // built in service need to register (optional service)
@@ -23,24 +23,51 @@ namespace WebApp
             builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();//register
             builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();//register
             builder.Services.AddScoped<ITestService, TestService>();//register
+            #endregion
 
             var app = builder.Build();
+            #region Inline MidldewareCustom
+            //app.Use(async (ctx, next) => {
+            //    await  ctx.Response.WriteAsync("1- Middleware 1\n");//<--1
+            //    await next.Invoke();                                //<--2
+            //    await ctx.Response.WriteAsync("1-1 Middleware 1-1\n");//<--7
 
+            //});
+            //app.Use(async (ctx, next) => {
+            //    await ctx.Response.WriteAsync("2- Middleware 2\n");//<--3
+            //    await next.Invoke();                               //<--4
+            //    await ctx.Response.WriteAsync("2-2 Middleware 2-2\n");//<--6
+            //});
+            //app.Run(async(ctx) =>
+            //{
+            //    await ctx.Response.WriteAsync("3- Termiate\n");//<--5
+            //});
+            #endregion
+
+            #region built in Middleware
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            app.UseStaticFiles();
+            app.UseStaticFiles();//path wwroot call next
 
-            app.UseRouting();
+            app.UseRouting();//Mapping Security
 
             app.UseAuthorization();
+
+            //Declare Route and execute
+            //app.MapControllerRoute("Route1", "r1/{id:int:range(20,60)}/{name?}", new { controller = "Route", action = "Method1" });//
+            //app.MapControllerRoute("Route1", "r1", new { controller = "Route", action = "Method1" });//
+            //app.MapControllerRoute("Route2", "r2", new { controller = "Route", action = "Method2" });
+            //app.MapControllerRoute("Route2", "{contoller}/{action}", new { controller = "Route", action = "Method2" });
+
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
+            #endregion
             app.Run();
         }
     }
